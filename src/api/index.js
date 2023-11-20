395802ebc7a4a804b126a72690b85d44b2430113
DagoGres <dan333pro@gmai.com>
2023-11-20 11:24:20 -0500

    Cors abiertos temporalmente, hasta que se despliegue el Front en una solución serverless

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const swaggerUi = require('swagger-ui-express');
 
@@ -8,6 +9,7 @@ const router = require('./network');
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 
 // ROUTER
